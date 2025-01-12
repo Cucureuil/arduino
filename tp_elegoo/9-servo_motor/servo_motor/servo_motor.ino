386ac81e1d4876131953cc159bb77e3bbb0606de
cucureuil <masoni.chloe282@gmail.com>
2025-01-12 21:49:23 +0100

    feat: tp 9 - servo motor

---
+++ tp_elegoo/9-servo_motor/servo_motor/servo_motor.ino
@@ -0,0 +1,31 @@
+#include <Servo.h>
+
+const Servo myServo;
+
+const int SERVO_PIN = 9;
+
+void setup() {
+  pinMode(SERVO_PIN, OUTPUT);
+  myServo.attach(SERVO_PIN);
+  myServo.write(0);
+}
+
+void loop() {
+  for (int i=0;i<=180;i++) {
+    myServo.write(i);
+    delay(5);
+  }
+   for (int i=180;i>=0;i--) {
+    myServo.write(i);
+    delay(5);
+  }
+  delay(1000);
+  myServo.write(50);
+  delay(100);
+  myServo.write(100);
+  delay(100);
+  myServo.write(150);
+  delay(100);
+  myServo.write(0);
+  delay(1000);
+}
